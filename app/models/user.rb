11a06adc0b8aed8e9ec3e53639aa38ec605fd42b
gurani115 <kouki11x@gmail.com>
2021-04-24 13:51:07 +0900

    チャットルームの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6 }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
 
-  has_many :room_users
-  has_many :rooms, through: :room_users
+  
+  has_many :rooms
 end
